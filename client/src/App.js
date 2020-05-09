11bb59c648938f89a01b11d541097f4b7c782e00
livany.tavares <livany.tavares@clubpetro.com>
2020-05-08 23:35:44 -0300

    Added Styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { Home } from "./pages/Home";
-import "./App.css";
+import "./assets/styles.css";
 import { ThemeProvider } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import theme from "./theme";
+import theme from "./assets/theme";
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  height: 100vh;
+  width: 100vw;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <AppStyled>
       <header></header>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Home />
       </ThemeProvider>
-    </div>
+    </AppStyled>
   );
 }
 
